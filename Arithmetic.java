47a6fc51a63eb76a03dfb71a33e3302a9982676d
MANU K SHIBU <76098146+manu434@users.noreply.github.com>
2025-01-13 19:46:14 +0530

    Arithmetic

---
+++ Arithmetic.java
@@ -0,0 +1,27 @@
+package basics;
+
+public class Arithmetic {
+   public static void main(String[] args)
+   {
+	   int a =10;
+	   int b =2;
+	    System.out.println("The First no: " + a);
+	    System.out.println("The second no: " + b);
+	  
+	   int Addition=a+b;
+	   System.out.println("Addition Result: " + Addition);
+	   
+	   int Substraction=a-b;
+	   System.out.println("Substractio rssult: " + Substraction);
+	   
+	   int Division = a/b;
+	   System.out.println("Division Result: " + Division);
+	   
+	   int Multi=a*b;
+	   System.out.println("Multiplication Result: " + Multi);
+	   
+	   int modulo=a%b;
+	   System.out.println("modulo Result: " + modulo);
+	   
+   }
+}
