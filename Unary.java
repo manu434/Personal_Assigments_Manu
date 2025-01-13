4d9e93ef66cd42c20f3db9fced1c694e85571374
MANU K SHIBU <76098146+manu434@users.noreply.github.com>
2025-01-13 19:54:25 +0530

    unary operator

---
+++ Unary.java
@@ -0,0 +1,26 @@
+package basics;
+
+public class Unary {
+ public static void main(String[] agrs) {
+	 //post_increment
+	 int a =60;
+	 System.out.println(a++);
+	 System.out.println(a);
+	 
+	 //post_decrement
+	 int b = 60;
+	 System.out.println(b--);
+	 System.out.println(b);
+	 
+	 //pre_increment
+	 int c =60;
+	 System.out.println(++c);
+	 System.out.println(c);
+	 
+	 //pre_decrement
+	 int d =60;
+	 System.out.println(--d);
+	 System.out.println(d);
+	 
+ }
+}
